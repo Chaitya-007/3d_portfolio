3c367e4c3f78dc0c68da8d5a45ae2e971f6e7c06
Chaitanya <chaitanyaasole@gmail.com>
2024-10-23 14:20:15 +0530

    imported BrowserRouter, and some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return <div>3d Developer Portfolio</div>;
