7cd29fe4e4cdd8d2f161ff85cd55f068d2c2bbba
Chaitanya <chaitanyaasole@gmail.com>
2024-10-23 16:16:22 +0530

    did styling of first element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      <div className="w-full flex justify-between items-center max-w-7xl max-auto ">
         <Link
           to="/"
           className="flex items-center gap-2"
@@ -22,10 +22,26 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Chaitya <span className="sm:block hidden">| Dream Works</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Chaitya&nbsp;<span className="sm:block hidden">| Dream Works</span>
           </p>
         </Link>
+        <ul
+          className="list-none
+         hidden sm:flex flex-row gap-10"
+        >
+          {navLinks.map((Link) => (
+            <li
+              key={Link.id}
+              className={`${
+                active === Link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
