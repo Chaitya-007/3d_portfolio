2cdde56f45cf0e7262809b1aeb7f9fe4ae4e1e7a
Chaitanya <chaitanyaasole@gmail.com>
2024-10-26 21:52:22 +0530

    Given the title and subtext

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,7 +8,14 @@ import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return <></>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+    </>
+  );
 };
 
 export default SectionWrapper(Works, "");
