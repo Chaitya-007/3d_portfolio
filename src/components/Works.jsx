03ecd387d91f3b8e5d87d2c286d64bd458ca9291
Chaitanya <chaitanyaasole@gmail.com>
2024-10-26 21:49:46 +0530

    Wrapped in section wrapper using react fragments

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,9 +5,10 @@ import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
-  return <div>Works</div>;
+  return <></>;
 };
 
-export default Works;
+export default SectionWrapper(Works, "");
