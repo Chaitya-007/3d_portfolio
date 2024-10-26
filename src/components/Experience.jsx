c484785939153cf50545b74389dd96b88d673ff9
Chaitanya <chaitanyaasole@gmail.com>
2024-10-26 20:09:05 +0530

    Designing Experience Card

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,6 +10,12 @@ import "react-vertical-timeline-component/style.min.css";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
 
+const ExperienceCard = ({ experience }) => {
+  <VerticalTimelineElement
+    contentStyle={{ background: "#1d1836", color: "#fff" }}
+  ></VerticalTimelineElement>;
+};
+
 const Experience = () => {
   return (
     <>
@@ -18,7 +24,13 @@ const Experience = () => {
         <h2 className={styles.sectionHeadText}>Work Experience</h2>
       </motion.div>
 
-      <div className="mt-20 flex flex-col"></div>
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   );
 };
