21226d88cfbad62a9d6f646416512b7645b79b37
Chaitanya <chaitanyaasole@gmail.com>
2024-10-26 16:54:21 +0530

    Designed Introductin and Overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4">
+        I am a software developer with a passion for building web applications
+        and exploring new technologies. I have experience with JavaScript,
+        React, Node.js, Express, and MongoDB. I am always eager to learn new
+        things and improve my skills.
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default About;
