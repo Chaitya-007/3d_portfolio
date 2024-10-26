3828aad6fb1af88a8fffb4265f70de191e874c87
Chaitanya <chaitanyaasole@gmail.com>
2024-10-26 21:16:35 +0530

    Adding animation to balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [texture] = useTexture([props.imgUrl]);
+  return <Float speed={1.75} rotationIntensity={1} floatIntensity={2}></Float>;
+};
+
+const BallCanvas = ({ icon }) => {
   return (
-    <div>Ball</div>
-  )
-}
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
 
-export default Ball
+export default BallCanvas;
