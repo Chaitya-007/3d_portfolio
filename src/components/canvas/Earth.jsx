2b8d163dfcc86342fbb4ec31d19c902aef086615
Chaitanya <chaitanyaasole@gmail.com>
2024-10-28 11:28:19 +0530

    Made EarthCanvas Function

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -9,7 +9,23 @@ const Earth = () => {
 };
 
 const EarthCanvas = () => {
-  return;
+  return (
+    <Canvas
+      shadows
+      frameLoop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+      </Suspense>
+    </Canvas>
+  );
 };
 
-export default Earth;
+export default EarthCanvas;
