43e957f3538bdf80d33f3f352ee2693513fec02c
Chaitanya <chaitanyaasole@gmail.com>
2024-10-28 10:19:03 +0530

    Started with contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import emailjs from "@emailjs/browser";
+import { Earthcanvas } from "./canvas";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  return <div>Contact</div>;
+};
 
-export default Contact
+export default Contact;
