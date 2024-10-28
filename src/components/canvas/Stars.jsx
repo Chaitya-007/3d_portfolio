234028477085bc3638468a31263a2252a7b3ae8d
Chaitanya <chaitanyaasole@gmail.com>
2024-10-28 12:36:40 +0530

    Made Stars but are static

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -4,13 +4,29 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
-  return <div>Stars</div>;
+const Stars = (props) => {
+  const ref = useRef();
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
 const StarsCanvas = () => {
   return (
-    <div className="w-full h-full absolute inset-0 z-[-1]">
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
